c9a57bd73f08be0a0e326fb47741514d826b84b7
Marvin Gudel <marvin.gudel@9elements.com>
2026-04-14 13:48:04 +0200

    Add demo peer cert store

--- target/ast1060-evb/spdm-req-resp-test/mock_platform.rs
+++ target/ast1060-evb/spdm-req-resp-test/mock_platform.rs
@@ -5,12 +5,17 @@
 //! Provides minimal implementations of SPDM platform traits without
 //! requiring hardware or IPC dependencies.
 
-use spdm_lib::cert_store::{CertStoreError, CertStoreResult, SpdmCertStore};
+use heapless::Vec;
+use pw_log::error;
+use spdm_lib::cert_store::{CertStoreError, CertStoreResult, PeerCertStore, SpdmCertStore};
+use spdm_lib::commands::challenge::MeasurementSummaryHashType;
 use spdm_lib::platform::evidence::{SpdmEvidence, SpdmEvidenceError, SpdmEvidenceResult};
 use spdm_lib::platform::hash::{SpdmHash, SpdmHashAlgoType, SpdmHashError, SpdmHashResult};
 use spdm_lib::platform::rng::{SpdmRng, SpdmRngResult};
 use spdm_lib::protocol::algorithms::{AsymAlgo, ECC_P384_SIGNATURE_SIZE, SHA384_HASH_SIZE};
 use spdm_lib::protocol::certs::{CertificateInfo, KeyUsageMask};
+use spdm_lib::protocol::{BaseHashAlgoType, SpdmCertChainHeader};
+use zerocopy::FromBytes;
 
 /// Mock certificate store with fixed placeholder data
 pub struct MockCertStore;
@@ -263,5 +268,315 @@ impl SpdmEvidence for MockEvidence {
         offset += 20;
 
         Ok(offset)
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct PeerSlot {
+    /// CertChain[K], retrieved in `CERTIFICATE` response.
+    pub cert_chain: Vec<u8, 1024>,
+
+    /// Digest[K], retrieved in `DIGESTS` response.
+    pub digest: Vec<u8, 48>,
+
+    /// `KeyPairID[K]`, retrieved in `DIGESTS` response if the corresponding `MULTI_KEY_CONN_REQ` or `MULTI_KEY_CONN_RSP` is true.
+    pub keypair_id: Option<u8>,
+
+    /// `CertificateInfo[K]`, retrieved in `DIGESTS` response if the corresponding `MULTI_KEY_CONN_REQ` or `MULTI_KEY_CONN_RSP` is true. pub cert_info: Option<CertificateInfo>
+    pub certificate_info: Option<CertificateInfo>,
+
+    /// KeyUsageMask[K], retrieved in `DIGESTS` response if the corresponding `MULTI_KEY_CONN_REQ` or `MULTI_KEY_CONN_RSP` is true.
+    pub key_usage_mask: Option<KeyUsageMask>,
+
+    pub requested_msh_type: Option<MeasurementSummaryHashType>,
+}
+
+impl PeerSlot {
+    /// Get the digest for the root certificate of the chain
+    ///
+    /// # Arguments
+    /// * `hash_algo` - The hash algorithm negotiated with the peer.
+    fn get_root_hash(&self, hash_algo: BaseHashAlgoType) -> Option<&[u8]> {
+        let (length, rest) = SpdmCertChainHeader::ref_from_prefix(&self.cert_chain).ok()?;
+        if length.get_length() != self.cert_chain.len() as u32 {
+            error!("cert chain length mismatch");
+            return None;
+        }
+        Some(&rest[..hash_algo.hash_byte_size()])
+    }
+    /// Get the DER x509 certificate chain
+    ///
+    /// # Arguments
+    /// * `hash_algo` - The hash algorithm negotiated with the peer.
+    fn get_cert_chain(&self, hash_algo: BaseHashAlgoType) -> Option<&[u8]> {
+        let (length, rest) = SpdmCertChainHeader::ref_from_prefix(&self.cert_chain).ok()?;
+        if length.get_length() != self.cert_chain.len() as u32 {
+            error!("cert chain length mismatch");
+            return None;
+        }
+        Some(&rest[hash_algo.hash_byte_size()..])
+    }
+}
+
+/// Concrete implementation of `PeerCertStore` for demonstration purposes.
+/// This example store manages a single certificate slot (slot 0) and allows
+/// setting and retrieving the certificate chain, digest, key pair ID, certificate info,
+/// and key usage mask for that slot. In a real implementation, you would likely
+/// want to support multiple slots and have more robust error handling and storage mechanisms.
+#[derive(Debug)]
+pub struct DemoPeerCertStore {
+    /// Retrieved from `DIGESTS` response, indicates which certificate slots are supported by the peer.
+    supported_slots_mask: u8,
+
+    /// Retrieved from `DIGESTS` response, indicates which certificate slots are provisioned with valid certificate chains.
+    provisioned_slots_mask: u8,
+
+    // Since not all existing slots may hold eligible certificate chains, keep the PeerSlot values optional.
+    pub peer_slots: Vec<Option<PeerSlot>, 1>,
+}
+
+impl Default for DemoPeerCertStore {
+    fn default() -> Self {
+        let mut slots = Vec::new();
+        let _ = slots.push(None);
+        DemoPeerCertStore {
+            supported_slots_mask: 0,
+            provisioned_slots_mask: 0,
+            peer_slots: slots,
+        }
+    }
+}
+
+impl PeerCertStore for DemoPeerCertStore {
+    fn slot_count(&self) -> u8 {
+        self.peer_slots.len() as u8
+    }
+
+    fn assemble(
+        &mut self,
+        slot_id: u8,
+        portion: &[u8],
+    ) -> Result<spdm_lib::cert_store::ReassemblyStatus, CertStoreError> {
+        let slot = self
+            .peer_slots
+            .get_mut(slot_id as usize)
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))?
+            .as_mut()
+            .ok_or(CertStoreError::PlatformError)?;
+
+        slot.cert_chain
+            .extend_from_slice(portion)
+            .map_err(|_| CertStoreError::BufferTooSmall)?;
+
+        Ok(spdm_lib::cert_store::ReassemblyStatus::InProgress)
+    }
+
+    fn reset(&mut self, slot_id: u8) {
+        if let Some(Some(slot)) = self.peer_slots.get_mut(slot_id as usize) {
+            *slot = PeerSlot::default();
+        }
+    }
+
+    fn get_raw_chain(&self, slot_id: u8) -> CertStoreResult<&[u8]> {
+        let slot = self
+            .peer_slots
+            .get(slot_id as usize)
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))?
+            .as_ref()
+            .ok_or(CertStoreError::PlatformError)?;
+        Ok(&slot.cert_chain)
+    }
+
+    fn get_cert_chain(&self, slot_id: u8, hash_algo: BaseHashAlgoType) -> CertStoreResult<&[u8]> {
+        let slot = self
+            .peer_slots
+            .get(slot_id as usize)
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))?
+            .as_ref()
+            .ok_or(CertStoreError::PlatformError)?;
+        slot.get_cert_chain(hash_algo)
+            .ok_or(CertStoreError::CertReadError)
+    }
+
+    /// Set the supported slots bit mask and initialize PeerSlot entries for any newly supported slots.  
+    fn set_supported_slots(&mut self, slot_mask: u8) -> CertStoreResult<()> {
+        for b in 0..8 {
+            if slot_mask & (1 << b) == 1 {
+                if let Some(slot) = self.peer_slots.get_mut(b as usize) {
+                    if slot.is_none() {
+                        *slot = Some(PeerSlot::default());
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn get_supported_slots(&self) -> CertStoreResult<u8> {
+        Ok(self.supported_slots_mask)
+    }
+
+    fn set_provisioned_slots(&mut self, provisioned_slot_mask: u8) -> CertStoreResult<()> {
+        self.provisioned_slots_mask = provisioned_slot_mask;
+        Ok(())
+    }
+
+    fn get_provisioned_slots(&self) -> CertStoreResult<u8> {
+        Ok(self.provisioned_slots_mask)
+    }
+
+    /// Set the certificate chain for a given slot. This would typically be called
+    /// after successfully reassembling the certificate chain from received portions.
+    ///
+    /// # Returns
+    /// - `Ok(())` if the certificate chain was set successfully
+    /// - `Err(CertStoreError)` if there was an error (e.g., invalid slot ID)
+    fn set_cert_chain(&mut self, slot_id: u8, cert_chain: &[u8]) -> CertStoreResult<()> {
+        let slot = self
+            .peer_slots
+            .get_mut(slot_id as usize)
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))?
+            .as_mut()
+            .ok_or(CertStoreError::PlatformError)?;
+
+        slot.cert_chain = cert_chain
+            .try_into()
+            .map_err(|_| CertStoreError::BufferTooSmall)?;
+        Ok(())
+    }
+
+    fn get_digest(&self, slot_id: u8) -> CertStoreResult<&[u8]> {
+        let slot = self
+            .peer_slots
+            .get(slot_id as usize)
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))?
+            .as_ref()
+            .ok_or(CertStoreError::PlatformError)?;
+        Ok(&slot.digest)
+    }
+
+    /// Set the digest for a given slot, provided by the `DIGESTS` response.
+    ///
+    /// # Parameters
+    /// - `slot_id`: The slot ID to set the digest for
+    /// - `digest`: The digest value to set
+    fn set_digest(&mut self, slot_id: u8, digest: &[u8]) -> CertStoreResult<()> {
+        let slot: &mut PeerSlot = self
+            .peer_slots
+            .get_mut(slot_id as usize)
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))?
+            .as_mut()
+            .ok_or(CertStoreError::PlatformError)?;
+        slot.digest = digest
+            .try_into()
+            .map_err(|_| CertStoreError::BufferTooSmall)?;
+        Ok(())
+    }
+
+    fn get_cert_info(&self, slot_id: u8) -> CertStoreResult<CertificateInfo> {
+        let slot = self
+            .peer_slots
+            .get(slot_id as usize)
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))?
+            .as_ref()
+            .ok_or(CertStoreError::PlatformError)?;
+        slot.certificate_info
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))
+    }
+    fn set_cert_info(&mut self, slot_id: u8, cert_info: CertificateInfo) -> CertStoreResult<()> {
+        let slot = self
+            .peer_slots
+            .get_mut(slot_id as usize)
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))?
+            .as_mut()
+            .ok_or(CertStoreError::PlatformError)?;
+        slot.certificate_info = Some(cert_info);
+        Ok(())
+    }
+    fn get_key_usage_mask(&self, slot_id: u8) -> CertStoreResult<KeyUsageMask> {
+        let slot = self
+            .peer_slots
+            .get(slot_id as usize)
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))?
+            .as_ref()
+            .ok_or(CertStoreError::PlatformError)?;
+        slot.key_usage_mask
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))
+    }
+
+    fn set_key_usage_mask(
+        &mut self,
+        slot_id: u8,
+        key_usage_mask: KeyUsageMask,
+    ) -> CertStoreResult<()> {
+        let slot = self
+            .peer_slots
+            .get_mut(slot_id as usize)
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))?
+            .as_mut()
+            .ok_or(CertStoreError::PlatformError)?;
+        slot.key_usage_mask = Some(key_usage_mask);
+        Ok(())
+    }
+
+    fn get_keypair(&self, slot_id: u8) -> CertStoreResult<u8> {
+        let slot = self
+            .peer_slots
+            .get(slot_id as usize)
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))?
+            .as_ref()
+            .ok_or(CertStoreError::PlatformError)?;
+        slot.keypair_id
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))
+    }
+
+    fn set_keypair(&mut self, slot_id: u8, keypair: u8) -> CertStoreResult<()> {
+        let slot = self
+            .peer_slots
+            .get_mut(slot_id as usize)
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))?
+            .as_mut()
+            .ok_or(CertStoreError::PlatformError)?;
+        slot.keypair_id = Some(keypair);
+        Ok(())
+    }
+
+    fn get_root_hash(&self, slot_id: u8, hash_algo: BaseHashAlgoType) -> CertStoreResult<&[u8]> {
+        let slot = self
+            .peer_slots
+            .get(slot_id as usize)
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))?
+            .as_ref()
+            .ok_or(CertStoreError::PlatformError)?;
+        slot.get_root_hash(hash_algo)
+            .ok_or(CertStoreError::CertReadError)
+    }
+
+    fn get_requested_msh_type(&self, slot_id: u8) -> CertStoreResult<MeasurementSummaryHashType> {
+        self.peer_slots
+            .get(slot_id as usize)
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))?
+            .as_ref()
+            .ok_or(CertStoreError::PlatformError)?
+            .requested_msh_type
+            .clone()
+            .ok_or(CertStoreError::Undefined)
+    }
+
+    fn set_requested_msh_type(
+        &mut self,
+        slot_id: u8,
+        msh_type: MeasurementSummaryHashType,
+    ) -> CertStoreResult<()> {
+        let slot = self
+            .peer_slots
+            .get_mut(slot_id as usize)
+            .ok_or(CertStoreError::InvalidSlotId(slot_id))?
+            .as_mut()
+            .ok_or(CertStoreError::PlatformError)?;
+        slot.requested_msh_type = Some(msh_type);
+
+        Ok(())
     }
 }
