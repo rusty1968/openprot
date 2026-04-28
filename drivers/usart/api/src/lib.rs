dee13b283f85dd0db1a12709f754a53dd5f5c415
Anthony Rocha <anthony.rocha@amd.com>
2026-04-28 12:48:24 -0700

    Usart: user space driver crates, docs, and async try-read multi-client QEMU flow

---
+++ drivers/usart/api/src/lib.rs
@@ -0,0 +1,8 @@
+// Licensed under the Apache-2.0 license
+
+#![no_std]
+
+pub mod backend;
+pub mod protocol;
+
+pub use protocol::*;
---
+++ target/ast10x0/peripherals/lib.rs
@@ -0,0 +1,6 @@
+// Licensed under the Apache-2.0 license
+// SPDX-License-Identifier: Apache-2.0
+
+#![no_std]
+
+pub mod uart;
