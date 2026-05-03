5e45cda381451148e012b6e0c5a823ccc337f0a7
Anthony Rocha <anthony.rocha@amd.com>
2026-05-03 15:17:07 -0700

    target/ast10x0: remove host-only SMC tests

--- target/ast10x0/peripherals/helpers_host_test.rs
+++
@@ -1,7 +0,0 @@
-// Licensed under the Apache-2.0 license
-// SPDX-License-Identifier: Apache-2.0
-
-#[path = "smc/host_mod.rs"]
-mod smc;
-
-fn main() {}
--- target/ast10x0/peripherals/smc/host_mod.rs
+++
@@ -1,8 +0,0 @@
-// Licensed under the Apache-2.0 license
-// SPDX-License-Identifier: Apache-2.0
-
-#[path = "types.rs"]
-pub mod types;
-
-#[path = "helpers.rs"]
-pub mod helpers;
